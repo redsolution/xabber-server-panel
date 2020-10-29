177a82c04bb0b91f1882b6c24544c173855c8d30
egor.merkushkin <egor.merkushkin@redsolution.ru>
2020-10-29 10:57:28 +0500

    templatetag to show subitems defined in module specs

--- modules_installation/templatetags/modules_tags.py
+++ modules_installation/templatetags/modules_tags.py
@@ -15,3 +15,25 @@ def get_modules():
             'url': reverse('modules:%s:info' % module['name'])
         })
     return list
+
+
+@register.simple_tag
+def get_menu_subitems():
+    subitems = []
+
+    for module in settings.MODULES_SPECS:
+        for menu_item in module['menu_subitems']:
+            for subitem in module['menu_subitems'][menu_item]:
+                subitems.append({
+                    'menu_item': menu_item,
+                    'subitem': subitem['name'],
+                    'url': reverse('modules:%s:%s' % (module['name'], subitem['url_name']))
+                })
+        for subitem in module['create_items']:
+            print(subitem)
+            subitems.append({
+                'menu_item': 'create',
+                'subitem': subitem['name'],
+                'url': reverse('modules:%s:%s' % (module['name'], subitem['url_name']))
+            })
+    return subitems
