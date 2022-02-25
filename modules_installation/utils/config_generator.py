1470d97cc63f2b610941b33ce48376bc1b8db1f2
evgeniy.hohryakov <evgeniy.hohryakov@redsolution.ru>
2022-02-25 11:11:31 +0500

    fix update modules config file

--- modules_installation/utils/config_generator.py
+++ modules_installation/utils/config_generator.py
@@ -6,7 +6,7 @@ from django.apps import apps
 def update_modules_config_file():
     configs = []
     for name, app in apps.app_configs.items():
-        if app.module.config is not None:
+        if hasattr(app.module, 'config') and app.module.config is not None:
             configs.append(app.module.config.get_module_config())
     config_list = '\n'.join(configs)
     file = open(os.path.join(settings.EJABBERD_CONFIG_PATH,
