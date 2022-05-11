d162ee78440e2ced78bde3f040873c5c38baafce
cheeoreeo <80579324+cheeoreeo@users.noreply.github.com>
2022-05-11 14:35:01 +0500

    Registration and modules config (#5)
    Сhanging the registration page

--- registration/models.py
+++ registration/models.py
@@ -5,5 +5,9 @@ from virtualhost.models import VirtualHost
 
 class RegistrationSettings(models.Model):
     vhost = models.ForeignKey(VirtualHost, on_delete=models.CASCADE)
-    is_enabled = models.BooleanField(default=False)
-    is_enabled_by_key = models.BooleanField(default=False)
+    status = models.CharField(default='disabled', max_length=50)
+
+
+class RegistrationURL(models.Model):
+    vhost = models.ForeignKey(VirtualHost, on_delete=models.CASCADE)
+    value = models.CharField(max_length=150)
