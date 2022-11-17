2495fe7c0258a3932d2fe203dab953ecc335220e
Ilya Kalashnikov <ilya.kalashnikov@redsolution.ru>
2022-11-17 16:23:49 +0500

    Added JWT verification to webhooks.

--- webhooks/utils.py
+++ webhooks/utils.py
@@ -1,17 +1,62 @@
 import hashlib
 import hmac
+import base64
+import json
+import time
+
 from django.conf import settings
 
 
 def check_signature(request):
     signature = request.headers.get(settings.WEBHOOKS_SIGNATURE_HEADER)
+    if not signature:
+        return check_jwt(request)
     key = settings.WEBHOOKS_SECRET
-    if key is None or not signature:
+    if key is None:
         return False
     key = key.encode()
     body = request.body
     body_hash = hmac.new(key, body, hashlib.sha256).hexdigest()
     return body_hash == signature
+
+
+def _extract_jwt(auth_header):
+    try:
+        auth_header_list = auth_header.split()
+        if auth_header_list[0] != 'Bearer':
+            return None
+        token = auth_header_list[1].split('.')
+        return dict(header=token[0], payload=token[1], signature=token[2])
+    except:
+        return None
+
+
+def _to_json(b64str):
+    s = b64str + '=' * (-len(b64str) % 4)
+    decoded = base64.urlsafe_b64decode(s).decode()
+    try:
+        return json.loads(decoded)
+    except:
+        return None
+
+
+def check_jwt(request):
+    key = settings.WEBHOOKS_SECRET
+    if key is None:
+        return False
+    token = _extract_jwt(request.headers.get('Authorization'))
+    if token is None:
+        return False
+    header = _to_json(token['header'])
+    if header['alg'] != 'HS256':
+        return False
+    payload = _to_json(token['payload'])
+    iat = payload.get('iat', False)
+    if not iat or (int(time.time()) - iat) > 60:
+        return False
+    digest = hmac.new(key.encode(), (token['header'] + '.' + token['payload']).encode(), hashlib.sha256).digest()
+    signature = base64.urlsafe_b64encode(digest).decode().replace('=', '')
+    return signature == token['signature']
 
 
 class WebHookResponse(Exception):
