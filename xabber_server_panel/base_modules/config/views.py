edd835474776e8b55050a703f3822e1f96cfce7a
ilya.basyrov <ilya.basyrov@redsolution.ru>
2025-08-15 12:31:14 +0500

    confirm code view fix

--- xabber_server_panel/base_modules/config/views.py
+++ xabber_server_panel/base_modules/config/views.py
@@ -1013,10 +1013,10 @@ class ConfirmXabberServices(LoginRequiredMixin, View):
         jid = result.get('full_jid')
         response = xservices_api.xmpp_auth(jid, code)
 
-        if xservices_api.errors:
+        if xservices_api.errors or not response.ok:
             return JsonResponse(
                 {
-                    "message": 'Confirm code service error.'
+                    "message": 'Confirm code error.'
                 },
                 status=500
             )
