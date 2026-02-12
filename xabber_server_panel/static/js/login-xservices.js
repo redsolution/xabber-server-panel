483344ea95b46d7537745710c2bfc318d6b4f28d
ilya.basyrov <ilya.basyrov@redsolution.ru>
2026-02-12 17:41:11 +0500

    polling xservices code replaced

--- xabber_server_panel/static/js/login-xservices.js
+++ xabber_server_panel/static/js/login-xservices.js
@@ -164,57 +164,6 @@ $(function () {
         });
     });
 
-    // SERVICES POLLING
-    function updateModulesData(){
-        const loader_target = $('.table-adaptive');
-        const data_target = $('.modules-data-js');
-
-        addLoader(loader_target);
-        if (data_target.length){
-            const url = window.location.pathname;
-
-            $.ajax({
-                url: url, // Replace with your URL
-                type: 'GET',
-                success: function(response) {
-                    let data = response.html
-                    if (data){
-                        data_target.html(data);
-                    }
-                    
-                    deleteLoader(loader_target);
-                },
-                error: function(xhr, status, error) {
-                    deleteLoader(loader_target);
-                }
-            });
-        }
-        else {
-            window.location.reload()
-        }
-    }
-
-    let previousData = null; // Store previous response
-
-    function servicesPolling() {
-        $.ajax({
-            url: '/api/services_hash/', // Replace with your URL
-            type: 'GET',
-            success: function(response) {
-                let services_hash = response.services_hash;
-                // Compare with previous data
-                if (previousData && services_hash && previousData != services_hash){
-                    updateModulesData();
-                }
-
-                // Update previousData for next comparison
-                previousData = services_hash;
-            }
-        });
-    }
-
-    setInterval(servicesPolling, 3000);
-
     document.addEventListener('click', async function(e) {
         const el = e.target.closest('.check-internet-connection');
         if (!el) return;
